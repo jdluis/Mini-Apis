2f5fcc348783b361efdc34d8f317870e6040d789
jdluis <jdluisdev@gmail.com>
2022-09-20 17:03:47 +0100

    update  refact

--- app.js
+++ app.js
@@ -1,70 +1,84 @@
-
 const OPTIONS = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '53e8757e87msh78be76563794de7p1dafb9jsn260ccbf1fcd9',
-		'X-RapidAPI-Host': 'ip-geolocation-ipwhois-io.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "53e8757e87msh78be76563794de7p1dafb9jsn260ccbf1fcd9",
+    "X-RapidAPI-Host": "ip-geolocation-ipwhois-io.p.rapidapi.com",
+  },
 };
 
-
-const RequestIp = async ip => {
-   try {
-    const res = await fetch(`https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/${ip}`, OPTIONS);
+const RequestIp = async (ip) => {
+  try {
+    const res = await fetch(
+      `https://ip-geolocation-ipwhois-io.p.rapidapi.com/json/${ip}`,
+      OPTIONS
+    );
     return await res.json();
   } catch (err) {
     return console.error(err);
   }
-}
+};
 
-    const $form = document.getElementById("form");
-    const $submitBtn = document.getElementById("submit");
-    const $input = document.querySelector('#ip');
+const $form = document.getElementById("form");
+const $submitBtn = document.getElementById("submit");
+const $input = document.querySelector("#ip");
 
-    //all results
-    const $elemToShow = document.getElementById("results");
-    //user info element
-    const $city =  document.getElementById("city");
-    const $location = document.getElementById("location");
-    const $postalCode = document.getElementById("code");
+//all results
+const elemToShow = document.getElementById("results");
 
-    //server info element
-    const $servCountry =  document.getElementById("servCountry");
-    const $company = document.getElementById("company");
-    const $domain = document.getElementById("domain");
+$form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const { value } = $input;
+  if (!value) return;
+
+  $submitBtn.setAttribute("disabled", "");
+  $submitBtn.setAttribute("aria-busy", "true");
+
+  const data = await RequestIp(value);
+
+  if (data) {
+    let pureJsonData = JSON.stringify(data, null, 2);
+    //User Info
+    $city = JSON.stringify(data.city);
+    let latitud = JSON.stringify(data.latitude);
+    let longitud = JSON.stringify(data.longitude);
+    $postalCode = JSON.stringify(data.country_phone);
+
+    //Server Info
+    $servCountry = JSON.stringify(data.country);
+    $company = JSON.stringify(data.org);
+    $domain = JSON.stringify(data.region);
+
+    elemToShow.innerHTML = `
+        <div>
+        <h2>Results:</h2>
+        <ul title="ip client info">
+          <h5>Ip owner info:</h5>
+          <li>City: ${$city}</li>
+          <li>lat: ${latitud} | long: ${longitud}</li>
+          <li>Country Phone: ${$postalCode}</li>
+        </ul>
   
-    $form.addEventListener ('submit', async (e) => {
-      e.preventDefault();
-      const {value} = $input;
-      if (!value) return;
-
-      $submitBtn.setAttribute('disabled', '');
-      $submitBtn.setAttribute('aria-busy', 'true');
-
-      const data = await RequestIp(value);
-
-      if (data) {
-        $elemToShow.innerHTML = JSON.stringify(data, null, 2);
-        console.log(data)
-
-        //User Info
-        $city.innerHTML = "City: " + JSON.stringify(data.city);
-        let latitud ="lat: " + JSON.stringify(data.latitude);
-        let longitud = "long: " + JSON.stringify(data.longitude);
-        $location.innerHTML = latitud + " | " + longitud;
-        $postalCode.innerHTML = "Country Phone: " + JSON.stringify(data.country_phone);
-
-        //Server Info
-        $servCountry.innerHTML = "Country: " + JSON.stringify(data.country);
-        $company.innerHTML = "Company: " + JSON.stringify(data.org);
-        $domain.innerHTML = "Region: " + JSON.stringify(data.region);
-
-      } else {
-        $elemToShow.innerHTML = "No Data";
-      }
-      $submitBtn.removeAttribute('disabled');
-      $submitBtn.removeAttribute('aria-busy');
-    })
-
-    
+        <ul title="ip client info">
+          <h5>Ip server info:</h5>
+          <li >Country: ${$servCountry}</li>
+          <li >Company: ${$company}</li>
+          <li >Region: ${$domain}</li>
+        </ul>
   
+        <div class="line"></div>
+        <input class="spoilerbutton" style="width:20%; min-width: 120px;" type="button" value="View Json..." onclick="this.value=this.value=='View Json...'?'Hide':'View Json...';">
+        <div class="spoiler">
+          <div>
+            <pre id="results">
+              ${pureJsonData}
+            </pre>
+          </div>
+        </div>
+      </div>
+        `;
+  } else {
+    $elemToShow.innerHTML = "No Data";
+  }
+  $submitBtn.removeAttribute("disabled");
+  $submitBtn.removeAttribute("aria-busy");
+});
